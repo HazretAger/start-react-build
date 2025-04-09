83fa4829be445b0b51695b39a99b87c877b09657
agerzhanokovKha <agerzhanokovkha@intech.rshb.ru>
2025-04-09 16:49:37 +0300

    added prettier

--- src/features/ThemeToggler/ui/ThemeToggler.tsx
+++ src/features/ThemeToggler/ui/ThemeToggler.tsx
@@ -1,17 +1,29 @@
-import { useEffect, useState } from "react"
-import { Moon, Sun } from "lucide-react";
-import Toggler from "@shared/ui/Toggler/Toggler";
+import { useEffect, useState } from 'react';
+import { Moon, Sun } from 'lucide-react';
+import Toggler from '@shared/ui/Toggler/Toggler';
 
 const ThemeToggler = () => {
-    const [isDark, setIsDark] = useState<boolean>(false);
+  const [isDark, setIsDark] = useState<boolean>(false);
 
-    const handleChange = () => setIsDark(!isDark);
+  const handleChange = () => setIsDark(!isDark);
 
-    useEffect(() => {
-        document.body.setAttribute('data-theme', isDark ? 'dark' : 'light');
-    }, [isDark]);
+  useEffect(() => {
+    document.body.setAttribute('data-theme', isDark ? 'dark' : 'light');
+  }, [isDark]);
 
-    return <Toggler value={isDark} icon={isDark ? <Moon color="#171A1C" size="20" /> : <Sun color="#171A1C" size="20" />} handler={handleChange} />
-}
+  return (
+    <Toggler
+      value={isDark}
+      icon={
+        isDark ? (
+          <Moon color="#171A1C" size="20" />
+        ) : (
+          <Sun color="#171A1C" size="20" />
+        )
+      }
+      handler={handleChange}
+    />
+  );
+};
 
 export default ThemeToggler;
